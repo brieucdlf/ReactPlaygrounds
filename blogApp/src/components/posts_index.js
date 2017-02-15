292c5a8f3dc1a5dc0e9e53c4a90c87c53ef1569c
brieucdlf <brieuc.dlf@icloud.com>
2017-02-15 10:50:56 +0100

    Update blog app

---
+++ blogApp/src/components/posts_index.js
@@ -0,0 +1,22 @@
+import React, { Component} from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchPosts } from '../actions/index';
+
+class PostsIndex extends Component {
+  componentWillMount() {
+    this.props.fetchPosts();
+  }
+
+  render() {
+    return (
+      <div>List of blog posts</div>
+    );
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchPosts }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(PostsIndex);
